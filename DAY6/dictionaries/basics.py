91578ba9e381c50aa1a8e85c36d70d2ac92f804e
darkdamater <darkdamater@gmail.com>
2026-01-28 03:28:57 +0530

    Add Day 6 Dictionaries basics practice programs

--- DAY6/dictionaries/basics.py
+++ DAY6/dictionaries/basics.py
@@ -1,1 +1,33 @@
 #dictionaries are used in py to store key value pairs 
+students={
+    "name":"Ankush",
+    "age":21,
+    "branch":"cse",
+}
+#accessing the values
+print(students["name"])
+print(students["branch"])
+
+#adding and updating a value
+students["college"]="xyz"
+students["age"]=33
+
+print(students)
+
+#looping in dictionary
+for key in students:
+    print(key)
+
+for value in students.values():
+    print(value)
+
+#looping through key value pair
+
+for key, value in students.items():
+    print(key, value)
+
+#check if the key exists
+if "name" in students:
+    print("Key exists")
+
+
